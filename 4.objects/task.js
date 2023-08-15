3c3337e73668730b4d9803b7eacfeb29df0480bf
Andrei Bezrukov <bezrukov.andrey86@gmail.com>
2023-08-15 16:10:06 +0500

    Add task

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,36 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if ('marks' in this === true) {
+    this.marks.push(...marks);
+  }
 }
 
 Student.prototype.getAverage = function () {
-  
+  if ('marks' in this === false || this.marks.length === 0) {
+  return 0;
+  }
+  const gradeAverage = this.marks.reduce((acc, mark, index, arr) => {
+  acc += mark;
+  if (index === arr.length - 1) {
+    return acc / arr.length;
+  }
+  return acc;
+  }, 0);
+  return gradeAverage;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
 }
