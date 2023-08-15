e257e1823529e8ffda4a1d53697c59ef659382df
Andrei Bezrukov <bezrukov.andrey86@gmail.com>
2023-08-15 16:16:52 +0500

    Add tasks

---
+++ 5.classes/task.js
@@ -0,0 +1,98 @@
+class PrintEditionItem {
+  constructor(name, releaseDate, pagesCount) {
+    this.name = name;
+    this.releaseDate = releaseDate;
+    this.pagesCount = pagesCount;
+    this.state = 100;
+    this.type = null;
+  }
+
+  set state(number) {
+    if (number < 0) {
+      this._state = 0;
+    } else if (number > 100) {
+      this._state = 100;
+    } else {
+      this._state = number;
+    }
+  }
+
+  get state() {
+    return this._state;
+  }
+
+  fix() {
+    this.state *= 1.5;
+  }
+}
+
+class Magazine extends PrintEditionItem {
+  constructor(name, releaseDate, pagesCount) {
+    super(name, releaseDate, pagesCount);
+    this.type = "magazine";
+  }
+}
+
+class Book extends PrintEditionItem {
+  constructor(author, name, releaseDate, pagesCount) {
+    super(name, releaseDate, pagesCount);
+    this.author = author;
+    this.type = "book";
+  }
+}
+
+class NovelBook extends Book {
+  constructor(author, name, releaseDate, pagesCount) {
+    super(author, name, releaseDate, pagesCount);
+    this.type = "novel";
+  }
+}
+
+class FantasticBook extends Book {
+  constructor(author, name, releaseDate, pagesCount) {
+    super(author, name, releaseDate, pagesCount);
+    this.type = "fantastic";
+  }
+}
+
+class DetectiveBook extends Book {
+  constructor(author, name, releaseDate, pagesCount) {
+    super(author, name, releaseDate, pagesCount);
+    this.type = "detective";
+  }
+}
+
+class Library {
+  constructor(name, books) {
+    this.name = name;
+    this.books = [];
+  }
+
+  addBook(book) {
+    if (book.state > 30) {
+      this.books.push(book);
+    }
+  }
+
+  findBookBy(type, value) {
+    for (const book of this.books) {
+      if (book[type] === value) {
+        return book;
+      }
+    }
+    return null;
+  }
+
+  giveBookByName(bookName) {
+
+    for (let element of this.books) {
+      if (element.name === bookName) {
+        const book = element;
+        this.books.splice(this.books.indexOf(element), 1);
+        return book;
+      }
+
+    }
+    return null;
+  }
+}
