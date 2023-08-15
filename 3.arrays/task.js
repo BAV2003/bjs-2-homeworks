a89317ad8c73f23b87979efe6e5def0d513ddeb9
Andrei Bezrukov <bezrukov.andrey86@gmail.com>
2023-08-15 16:02:55 +0500

    Add tasks

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,13 @@
 function compareArrays(arr1, arr2) {
-  
+  return ((arr1.length === arr2.length) && arr1.every((element, index) => element === arr2[index]));
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  return result = users.filter(user => user.gender === gender).map(years => years.age).reduce((acc, years, index, arr) => {
+    acc + years;
+    if (index === arr.length - 1) { 
+        return (acc + years) / arr.length;
+    }
+    return acc + years;
+  }, 0);
 }
